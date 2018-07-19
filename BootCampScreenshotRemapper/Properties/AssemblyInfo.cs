a44b349fb0b5b66de3549465ebcd01c09db79e44
Kyujin Cho <thy2134@gmail.com>
2018-07-20 02:37:37 +0900

    Change Assembly Info

--- BootCampScreenshotRemapper/Properties/AssemblyInfo.cs
+++ BootCampScreenshotRemapper/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BootCampScreenshotRemapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bring back macOS Screenshot Shortcut")]
+[assembly: AssemblyDescription("https://github.com/thy2134/BringBackMacScreenshot")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BootCampScreenshotRemapper")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Kyujin Cho")]
+[assembly: AssemblyProduct("Bring back macOS Screenshot Shortcut")]
+[assembly: AssemblyCopyright("Copyright Kyujin Cho ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
